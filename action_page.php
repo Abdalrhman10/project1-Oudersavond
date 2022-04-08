bbc5813f5df5e08ca690545c973d03871828cffb
Abdalrhman10 <67341893+Abdalrhman10@users.noreply.github.com>
2022-04-08 13:35:17 +0200

    Programma toegevoegd

---
+++ action_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Oudersavond</title>
+</head>
+<?php 
+$naam=$_POST["fname"];
+$email=$_POST["lname"];
+
+
+echo "<b>Naam:</b> $fname</br>";
+echo "<b>E-mail:</b> $iname</br>";
+?>
